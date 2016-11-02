372745aa7def9f68798528869ae2bd8dcc673259
Tobias Kuhn <kuhntobias@gmail.com>
2016-11-02 08:10:28 +0100

    Read and write nanopubs

--- src/main/java/org/petapico/npop/Run.java
+++ src/main/java/org/petapico/npop/Run.java
@@ -1,9 +1,77 @@
 package org.petapico.npop;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.GZIPOutputStream;
+
+import net.trustyuri.TrustyUriException;
+import net.trustyuri.TrustyUriResource;
+
+import org.nanopub.MalformedNanopubException;
+import org.nanopub.MultiNanopubRdfHandler;
+import org.nanopub.MultiNanopubRdfHandler.NanopubHandler;
+import org.nanopub.Nanopub;
+import org.nanopub.NanopubImpl;
+import org.nanopub.NanopubUtils;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.RDFParseException;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
 public class Run {
 
+	@com.beust.jcommander.Parameter(description = "input-nanopubs", required = true)
+	private List<File> inputNanopubs = new ArrayList<File>();
+
 	public static void main(String[] args) {
-		System.out.println( "Hello World!" );
+		NanopubImpl.ensureLoaded();
+		Run obj = new Run();
+		JCommander jc = new JCommander(obj);
+		try {
+			jc.parse(args);
+		} catch (ParameterException ex) {
+			jc.usage();
+			System.exit(1);
+		}
+		try {
+			obj.run();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	private void run() throws IOException, RDFParseException, RDFHandlerException,
+			MalformedNanopubException, TrustyUriException {
+		for (File inputFile : inputNanopubs) {
+			File outFile = new File(inputFile.getParent(), "op." + inputFile.getName());
+			final OutputStream out;
+			if (inputFile.getName().matches(".*\\.(gz|gzip)")) {
+				out = new GZIPOutputStream(new FileOutputStream(outFile));
+			} else {
+				out = new FileOutputStream(outFile);
+			}
+			final RDFFormat format = new TrustyUriResource(inputFile).getFormat(RDFFormat.TRIG);
+			MultiNanopubRdfHandler.process(format, inputFile, new NanopubHandler() {
+
+				@Override
+				public void handleNanopub(Nanopub np) {
+					try {
+						NanopubUtils.writeToStream(np, out, format);
+					} catch (RDFHandlerException ex) {
+						throw new RuntimeException(ex);
+					}
+				}
+
+			});
+			out.close();
+		}
 	}
 
 }
