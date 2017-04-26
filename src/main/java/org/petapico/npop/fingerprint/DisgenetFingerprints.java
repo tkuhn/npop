6d79d021d03463c60f5fbdf905d839a3dbfd3d71
Tobias Kuhn <kuhntobias@gmail.com>
2017-04-26 13:18:25 +0200

    Improve DisGeNET fingerprinting

--- src/main/java/org/petapico/npop/fingerprint/DisgenetFingerprints.java
+++ src/main/java/org/petapico/npop/fingerprint/DisgenetFingerprints.java
@@ -11,11 +11,22 @@ import org.nanopub.Nanopub;
 import org.nanopub.NanopubUtils;
 import org.openrdf.model.Resource;
 import org.openrdf.model.Statement;
-import org.openrdf.model.impl.StatementImpl;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.model.impl.ContextStatementImpl;
 import org.openrdf.model.impl.URIImpl;
 import org.petapico.npop.Fingerprint.FingerprintHandler;
 
 public class DisgenetFingerprints implements FingerprintHandler {
+
+	private static final URI assertionUriPlaceholder = new URIImpl("http://purl.org/nanopub/placeholders/assertion");
+	private static final URI provUriPlaceholder = new URIImpl("http://purl.org/nanopub/placeholders/provenance");
+	private static final URI timestampPlaceholder = new URIImpl("http://purl.org/nanopub/placeholders/timestamp");
+	private static final URI disgenetVoidPlaceholder = new URIImpl("http://purl.org/nanopub/placeholders/disgenet-void");
+	private static final URI disgenetGdaPlaceholder = new URIImpl("http://purl.org/nanopub/placeholders/disgenet-gda");
+
+	private static final URI pav1importedOn = new URIImpl("http://purl.org/pav/importedOn");
+	private static final URI pav2importedOn = new URIImpl("http://purl.org/pav/2.0/importedOn");
 
 	@Override
 	public String getFingerprint(Nanopub np) {
@@ -34,20 +45,47 @@ public class DisgenetFingerprints implements FingerprintHandler {
 		List<Statement> n = new ArrayList<>();
 		for (Statement st : statements) {
 			boolean isInAssertion = st.getContext().equals(np.getAssertionUri());
-			if (!isInAssertion) continue;
-			Resource subj = st.getSubject();
-			if (subj.stringValue().startsWith("http://rdf.disgenet.org/resource/gda/DGN") ||
-					subj.stringValue().startsWith("http://rdf.disgenet.org/gene-disease-association.ttl#DGN")) {
-				subj = new URIImpl("http://rdf.disgenet.org/resource/gda/DGN");
-				// ignore context:
-				st = new StatementImpl(subj, st.getPredicate(), st.getObject());			
+			boolean isInProvenance = st.getContext().equals(np.getProvenanceUri());
+			if (!isInProvenance && !isInAssertion) continue;
+			URI graphURI;
+			if (isInAssertion) {
+				graphURI = assertionUriPlaceholder;
 			} else {
-				// ignore context:
-				st = new StatementImpl(subj, st.getPredicate(), st.getObject());
+				graphURI = provUriPlaceholder;
 			}
-			n.add(st);
+			Resource subj = st.getSubject();
+			URI pred = st.getPredicate();
+			Value obj = st.getObject();
+			if (isInAssertion) {
+				String subjS = subj.stringValue();
+				if (subjS.startsWith("http://rdf.disgenet.org/resource/gda/DGN") ||
+						subjS.startsWith("http://rdf.disgenet.org/gene-disease-association.ttl#DGN")) {
+					subj = disgenetGdaPlaceholder;
+				}
+			} else if (isInProvenance) {
+				if (pred.equals(pav1importedOn) || pred.equals(pav2importedOn)) {
+					pred = pav2importedOn;
+					obj = timestampPlaceholder;
+				}
+				if (subj.equals(np.getAssertionUri())) {
+					subj = assertionUriPlaceholder;
+				}
+			}
+			n.add(new ContextStatementImpl((Resource) transform(subj), (URI) transform(pred), transform(obj), graphURI));
 		}
 		return n;
+	}
+
+	private Value transform(Value v) {
+		if (v instanceof URI) {
+			String s = ((URI) v).stringValue();
+			if (s.startsWith("http://rdf.disgenet.org/v")) {
+				return disgenetVoidPlaceholder;
+			} else if (s.startsWith("http://purl.obolibrary.org/obo/eco.owl#")) {
+				return new URIImpl(s.replace("http://purl.obolibrary.org/obo/eco.owl#", "http://purl.obolibrary.org/obo/"));
+			}
+		}
+		return v;
 	}
 
 }
